dcbfe6e2b35e2d03a02337ea3261de07a3f996e7
Artur Trąpczyński <trapczynski.a@gmail.com>
2021-02-04 18:18:26 +0100

    Fix add card problem

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -8,7 +8,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
   addCard: title => dispatch(createActionAddCard({
-    ColumnId: props.id,
+    columnId: props.id,
     title,
   })),
 });
